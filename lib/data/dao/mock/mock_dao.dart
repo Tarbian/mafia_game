764338391ee5d78a35759a85d548412055d8e467
Tarbian <oleh.vitruk.20@pnu.edu.ua>
2025-05-17 13:04:12 +0300

    fix typo

--- lib/data/dao/mock/mock_dao.dart
+++ lib/data/dao/mock/mock_dao.dart
@@ -1,7 +1,6 @@
 import 'package:Tests/domain/entities/mafiosnic.dart';
 
 abstract class MockDao {
-abstract class MockDao {
   //TODO: create data model "Mafiosnic"
 
   Future<List<Mafiosnic>> getMafiosnics();
